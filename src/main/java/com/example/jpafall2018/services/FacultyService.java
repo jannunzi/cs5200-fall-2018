bf790955e9d229f962e4307bd57868e11ba10f5b
jannunzi <jannunzi@gmail.com>
2018-10-24 09:17:24 -0400

    added findFacultyById

--- src/main/java/com/example/jpafall2018/services/FacultyService.java
+++ src/main/java/com/example/jpafall2018/services/FacultyService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.jpafall2018.models.Faculty;
@@ -16,5 +17,10 @@ public class FacultyService {
 	@GetMapping("/api/faculty")
 	public List<Faculty> findAllFaculty() {
 		return (List<Faculty>) fr.findAll();
+	}
+	@GetMapping("/api/faculty/{fid}")
+	public Faculty findFacultyById(
+			@PathVariable("fid") int fid) {
+		return fr.findById(fid).get();
 	}
 }
