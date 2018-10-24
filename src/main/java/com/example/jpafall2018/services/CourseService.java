0d05aaa64b8525cde20a9c1af1fe394f0f8c1a8b
jannunzi <jannunzi@gmail.com>
2018-10-24 08:41:55 -0400

    added deleteCourse

--- src/main/java/com/example/jpafall2018/services/CourseService.java
+++ src/main/java/com/example/jpafall2018/services/CourseService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,13 @@ public class CourseService {
 		Course course = courseRepository.findById(courseId).get();
 		course.setTitle(newCourse.getTitle());
 		courseRepository.save(course);
+		return (List<Course>) courseRepository.findAll();
+	}
+
+	@DeleteMapping("/api/course/{courseId}")
+	public List<Course> deleteCourse(
+			@PathVariable("courseId") int id) {
+		courseRepository.deleteById(id);
 		return (List<Course>) courseRepository.findAll();
 	}
 
